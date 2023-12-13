347e8eb3e676089bcf8ddca4bce46e2b1992b360
Rafael <francorfs@icloud.com>
2023-12-13 20:59:32 -0300

    Alteraçao da rota para teste

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './dto/entities/user';
 
 @Injectable()
 export class UsersService {
+  private readonly users: User[] = [];
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
