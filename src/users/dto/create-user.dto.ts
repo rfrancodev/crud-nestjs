99f05656c8dc1612b08fb49a03ef28697f2d6214
Rafael <francorfs@icloud.com>
2023-12-13 21:12:12 -0300

    Inserido campos para validar

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,6 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    name: string;
+    email: string
+}
+//Data Transfer Object
+//Objeto de transferencia de dados. Serve para validar os campos que viera  do body.
