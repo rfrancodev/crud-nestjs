c998185f934bdc85660247d61d0c5c0f21abb3ef
Rafael <francorfs@icloud.com>
2023-12-13 21:01:32 -0300

    Ajuste iniciais para realiar CRUD usando NEstJS

---
+++ src/users/dto/entities/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    private name: string;
+    private email: string;
+    constructor(name: string, email: string) {
+        this.name = name;
+        this.email = email;
+    }
+}
